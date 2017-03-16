3b8eab0adcba7b640191f8ff0f02a3fe36b3c4f2
Andrew Cookro <andrew.cookro@gmail.com>
2017-03-16 16:19:00 -0400

    Replace lorem ipsum text with real text content about the CrashCourse

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -26,17 +26,30 @@ export default class Homepage extends React.Component {
         </div>
         <div className="row main-content">
           <div className="col-md-4 text-left">
-              <h2>What is CrashCourse?</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+            <h2>What is CrashCourse?</h2>
+            <p>
+              CrashCourse is a web app for creating and taking custom, timed quizzes.
+              Test yourself or your friends on any subject matter you choose,
+              but be sure to make a choice before the answer blocks crash to the ground!
+            </p>
           </div>
           <div className="col-md-4 text-left">
-              <h2>Why use CrashCourse?</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+            <h2>Why use CrashCourse?</h2>
+            <p>
+              CrashCourse is fun and easy to use.
+              The timer, animation, and sound-effect features push the quiz taker to
+              answer as quickly as possible, training your brain to think quickly!
+            </p>
           </div>
           <div className="col-md-4 text-left">
-              <h2>Get started</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-              <Link to="/signup">Learn More</Link>
+            <h2>Get started</h2>
+            <p>
+              Creating a quiz is incredibly easy!
+              Click on Create Quiz tab, enter the name or subject of your quiz,
+              and then fill in the question and answers in the fields. To add more questions,
+              be sure to type in the same quiz name.
+            </p>
+            <Link to="/customQuiz">Create a quiz now!</Link>
           </div>
         </div>
 
